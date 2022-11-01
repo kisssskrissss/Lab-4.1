8c923d43016df5e785d787f9abdf46f5405eb733
krist <krist@LAPTOP-HIQLHLL6>
2022-11-02 01:16:12 +0500

    Добавьте файлы проекта.

---
+++ Lab 4.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи n");
+            int N= Convert.ToInt32(Console.ReadLine());
+            int s = 1;
+            for (int i = 1; i < 2*N -1; i+=2)
+            {
+                s += 2;
+                Console.WriteLine(" Сумма {0}", i);
+                s = s + i;
+            }
+            Console.WriteLine("Квадрат числа {0}", s);
+            Console.ReadKey();
+        }
+    }
+}
